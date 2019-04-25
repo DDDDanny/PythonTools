44bc423bcb8a7e6e52bdef8f1f77a4f4a0f147ad
DDDDanny <707956456@qq.com>
2019-04-25 09:29:35 +0800

    优化

--- AutoCreateData/running.py
+++ AutoCreateData/running.py
@@ -7,6 +7,6 @@
 from AutoCreateData.excel_save import DataInput
 
 
-num = int(input('请输入人数：'))
+cnt = int(input('请输入人数：'))
 Excel_File = ''
-DataInput(num, Excel_File).data_save()
+DataInput(cnt, Excel_File).data_save()
